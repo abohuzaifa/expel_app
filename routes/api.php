1be73a8975fffeec8761db31c6b5c7ea339102d3
iftikharali72232 <iftikharali72232@gmail.com>
2024-05-13 22:10:29 +0500

    push notification api

--- routes/api.php
+++ routes/api.php
@@ -87,6 +87,7 @@ Route::group(["middleware"=> "auth:sanctum"], function () {
     Route::post('/cardDetailUpdate/{id}', [AuthController::class, 'cardDetailUpdate'])->name('cardDetailUpdate');
     Route::post('/deleteCardDetails/{id}', [AuthController::class, 'deleteCardDetails'])->name('deleteCardDetails');
     Route::post('/updateVehicle', [AuthController::class, 'updateVehicle'])->name('updateVehicle');
+    Route::post('/send-notification', [AuthController::class, 'sendNotification'])->name('sendNotification');
 
 
     // Categories requests
