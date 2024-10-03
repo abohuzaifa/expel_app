90b884daeef1164e42a37efda0a9c8c5e2925ef9
iftikharali72232 <iftikharali72232@gmail.com>
2024-10-03 10:09:36 +0500

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -152,7 +152,98 @@ class User extends Authenticatable
         //     'notification' => $notification,
         //     'priority' => 'high'
         // ];
-//dN-4DUh1TamgfSsYKPvjM0:APA91bEOO5VxmPUDrI4kskY-LF7btvIoToiHEJ5mNYPd3SGU6ESsgcKD7oCCSXaFpeUSC27NPbZ8xSjPE6BsLScCSQjyVy6Dv0Ltp-PFDob_wGtGyt1PkVo6gnf6UsZKOAm1LAvBuwri
+        //dN-4DUh1TamgfSsYKPvjM0:APA91bEOO5VxmPUDrI4kskY-LF7btvIoToiHEJ5mNYPd3SGU6ESsgcKD7oCCSXaFpeUSC27NPbZ8xSjPE6BsLScCSQjyVy6Dv0Ltp-PFDob_wGtGyt1PkVo6gnf6UsZKOAm1LAvBuwri
+        $fields = '{
+            "message": {
+                 "token":"'.$data['device_token'].'",
+                 "notification":{
+                     "title":"'.$data['title'].'",
+                     "body":"'.$data['body'].'"
+                 },
+                 "data": {
+                    "request_id": "'.$data['request_id'].'"
+                }
+             }
+         }';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    
+        // print_r($ch); exit;
+        $result = curl_exec($ch);
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }
+        curl_close($ch);
+
+        return json_decode($result, true);
+    }
+
+    public static function sendPushNotification($data)
+    {
+        
+            $url = 'https://fcm.googleapis.com/v1/projects/opatra-d5bda/messages:send';
+
+            // Set your client credentials and refresh token
+            $client_id = getenv('GOOGLE_CLIENT_ID');
+            $client_secret = getenv('GOOGLE_CLIENT_SECRET');
+            $refresh_token = getenv('DRIVER_REFRESH_TOKEN'); // Replace with your actual refresh token
+        
+        // echo $url; exit;
+        $token_url = 'https://oauth2.googleapis.com/token';
+
+        // Prepare the POST fields
+        $post_fields = [
+            'client_id' => $client_id,
+            'client_secret' => $client_secret,
+            'refresh_token' => $refresh_token,
+            'grant_type' => 'refresh_token',
+        ];
+
+        // Initialize cURL
+        $ch = curl_init();
+
+        // Set the cURL options
+        curl_setopt($ch, CURLOPT_URL, $token_url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_fields));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/x-www-form-urlencoded'
+        ]);
+
+        // Execute the cURL request and get the response
+        $response = curl_exec($ch);
+
+        // Check for cURL errors
+        if (curl_errno($ch)) {
+            die('Curl error: ' . curl_error($ch));
+        }
+
+        // Close the cURL session
+        curl_close($ch);
+
+        // Decode the JSON response
+        $response_data = json_decode($response, true);
+        // print_r($response_data); exit;
+        // Check if the response contains an error
+        if (isset($response_data['error'])) {
+            die('Error refreshing the token: ' . $response_data['error']);
+        }
+
+        // print_r($response_data); exit;
+        // Extract the new access token
+        $newAccessToken = $response_data['access_token'];
+        $headers = [
+            'Authorization: Bearer '.$newAccessToken,
+            'Content-Type: application/json'
+        ];
+
         $fields = '{
             "message": {
                  "token":"'.$data['device_token'].'",
--- resources/views/outh.blade.php
+++ resources/views/outh.blade.php
@@ -8,8 +8,8 @@ if (!isset($_GET['code'])) {
 $authCode = $_GET['code'];
 
 // Set your client credentials and redirect URI
-$client_id = getenv('GOOGLE_CLIENT_ID_d');
-$client_secret = getenv('GOOGLE_CLIENT_SECRET_d');
+$client_id = getenv('GOOGLE_CLIENT_ID_junaid');
+$client_secret = getenv('GOOGLE_CLIENT_SECRET_junaid');
 $redirect_uri = 'http://localhost/expel_app/public/outh';
 $token_url = 'https://oauth2.googleapis.com/token';
 
@@ -82,4 +82,4 @@ $userInfo = json_decode($userInfoResponse, true);
 echo "User Info: <pre>" . print_r($userInfo, true) . "</pre>";
 ?>
 
-<!-- https://accounts.google.com/o/oauth2/auth?client_id=990392568575-jitq4hirb3vgph8evn1rsea9fhj3g33b.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%2Fexpel_app%2Fpublic%2Fouth&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Ffirebase.messaging&access_type=offline&prompt=consent&state=firebase_messaging -->
+<!-- https://accounts.google.com/o/oauth2/auth?client_id=1024991048436-it2tcsnnpoe4ulfgliaj45m0a9tq09pg.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%2Fexpel_app%2Fpublic%2Fouth&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Ffirebase.messaging&access_type=offline&prompt=consent&state=firebase_messaging -->
